d7f97ddb0250ad60407ca86241e736ae8a20a1c1
Mahmoud Samy <165038050+MahmooudSamy@users.noreply.github.com>
2025-03-04 22:55:40 +0200

    model wrapper  base caless

---
+++ Invento/Wrapper/ModelWrapper.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Invento.Wrapper
+{
+    public class ModelWrapper<T> : NotifayDataErrorBase
+    {
+        public ModelWrapper(T model)
+        {
+            Model = model;
+        }
+        public T Model { get; }
+
+        protected virtual TValue? GetValue<TValue>([CallerMemberName] string? propertyName = null)
+        {
+            if (propertyName == null) return default;
+
+            PropertyInfo? property = typeof(T).GetProperty(propertyName);
+            return property != null ? (TValue?)property.GetValue(Model) : default;
+        }
+        protected virtual void SetValue<TValue>(TValue value, [CallerMemberName] string? propertyName = null)
+        {
+            if (propertyName == null) return;
+
+            PropertyInfo? property = typeof(T).GetProperty(propertyName);
+            if (property != null)
+            {
+                property.SetValue(Model, value);
+                OnPropertyChanged(propertyName);
+                if (value != null)
+                {
+                    ValidatePropertyInternal(propertyName, value);
+                }
+
+            }
+        }
+
+        private void ValidatePropertyInternal(string propertyName, object currentValue)
+        {
+            ClearError(propertyName);
+            ValidateDataAnnoutation(propertyName, currentValue);
+            ValidateCustomErrors(propertyName);
+        }
+        private void ValidateDataAnnoutation(string propertyName, object currentValue)
+        {
+
+
+            if (Model != null)
+            {
+                var context = new ValidationContext(Model) { MemberName = propertyName };
+                var results = new List<ValidationResult>();
+                Validator.TryValidateProperty(currentValue, context, results);
+
+                foreach (var result in results)
+                {
+                    if (result.ErrorMessage != null)
+                    {
+                        AddError(propertyName, result.ErrorMessage);
+                    }
+                }
+            }
+
+        }
+
+        private void ValidateCustomErrors(string propertyName)
+        {
+            var errors = ValidateProperty(propertyName);
+            if (errors != null)
+            {
+                foreach (var error in errors)
+                {
+                    AddError(propertyName, error);
+                }
+            }
+        }
+
+        protected virtual IEnumerable<string>? ValidateProperty(string propertyName)
+        {
+            return null;
+        }
+
+
+    }
+}
---
+++ Invento/Wrapper/NotifayDataErrorBase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invento.Wrapper
+{
+    public class NotifayDataErrorBase : ViewModelBase, INotifyDataErrorInfo
+    {
+        private readonly Dictionary<string, List<string>> _errorsByPropertyName = new();
+
+
+
+        public bool HasErrors => _errorsByPropertyName.Any();
+
+        public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+        public IEnumerable GetErrors(string? propertyName)
+        {
+            if (propertyName == null)
+            {
+                return _errorsByPropertyName.Values.SelectMany(x => x);
+            }
+
+            if (_errorsByPropertyName.TryGetValue(propertyName, out var errors))
+            {
+                return errors;
+            }
+
+            return Enumerable.Empty<string>();
+        }
+
+        protected virtual void OnErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+            base.OnPropertyChanged(nameof(HasErrors));
+        }
+
+
+        protected void AddError(string propertyName, string error)
+        {
+            if (!_errorsByPropertyName.TryGetValue(propertyName, out var errors))
+            {
+                _errorsByPropertyName[propertyName] = new List<string>();
+            }
+
+            if (!_errorsByPropertyName[propertyName].Contains(error))
+            {
+                _errorsByPropertyName[propertyName].Add(error);
+                OnErrorsChanged(propertyName);
+            }
+        }
+
+        protected void ClearError(string propertyName)
+        {
+            if (_errorsByPropertyName.Remove(propertyName))
+            {
+                OnErrorsChanged(propertyName);
+            }
+        }
+
+    }
+}

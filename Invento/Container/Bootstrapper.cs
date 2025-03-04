3c4cd2d2547360230b25266d26cca5cd4abf25fb
Mahmoud Samy <165038050+MahmooudSamy@users.noreply.github.com>
2025-03-04 23:10:36 +0200

    Setup autofac make an container

---
+++ Invento/Container/Bootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using System.Windows;
+
+namespace Invento.Container
+{
+    public class Bootstrapper
+    {
+        public IContainer Bootstrap()
+        {
+            var bulider = new ContainerBuilder();
+
+           
+            return bulider.Build();
+        }
+    }
+}

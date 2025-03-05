2724e11f2c91b428b582f6ed34de6a71d278cc2f
Mahmoud Samy <165038050+MahmooudSamy@users.noreply.github.com>
2025-03-05 22:06:34 +0200

    Create Side Menu

--- Invento/MainWindow.xaml.cs
+++ Invento/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -19,6 +20,78 @@ namespace Invento
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private bool StateClosed = true;
+        private void PART_TITLEBAR_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void PART_CLOSE_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void PART_MAXIMIZE_RESTORE_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+            {
+                WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                Width = 1024;
+                Height = 768;
+                WindowState = WindowState.Normal;
+            }
+        }
+
+        private void PART_MINIMIZE_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void MainWindow1_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (WindowState == WindowState.Normal)
+                {
+                    WindowState = WindowState.Maximized;
+                }
+                else
+                {
+                    Width = 1024;
+                    Height = 768;
+                    WindowState = WindowState.Normal;
+                }
+            }
+            else
+            {
+                DragMove();
+            }
+        }
+
+     
+
+        private void ButtonMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (StateClosed)
+            {
+                Storyboard sb = FindResource("OpenMenu") as Storyboard;
+                sb.Begin();
+            }
+            else
+            {
+                Storyboard sb = FindResource("CloseMenu") as Storyboard;
+                sb.Begin();
+            }
+            StateClosed = !StateClosed;
         }
     }
 }

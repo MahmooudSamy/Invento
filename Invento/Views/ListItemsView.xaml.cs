de4cd188264add42876457f084431860027e39fc
Mahmoud Samy <165038050+MahmooudSamy@users.noreply.github.com>
2025-03-06 13:45:53 +0200

    Finish List Item

---
+++ Invento/Views/ListItemsView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Invento.Views
+{
+    /// <summary>
+    /// Interaction logic for ListItemsView.xaml
+    /// </summary>
+    public partial class ListItemsView : UserControl
+    {
+        public ListItemsView()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtSearch_Search(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

20bb686816e8f2df5cecb4a015a5a6823c0a3019
Mahmoud Samy <165038050+MahmooudSamy@users.noreply.github.com>
2025-03-07 15:34:41 +0200

    Create proceadure

---
+++ Invento.DataAccess/Migrations/20250307131348_CreateItemListProcedure.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Invento.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateItemListProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"create proc GetItems 
+As 
+BEGIN 
+Select Items.ItemId,ItemName,CategoryName,Quantity,LastUpdate
+From Items inner join Categories on Items.CategoryId=Categories.CategoryId
+inner join InventoryItems on Items.ItemId=InventoryItems.ItemId 
+END");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@" DROP PROCEDURE IF EXISTS GetItems");
+
+        }
+    }
+}

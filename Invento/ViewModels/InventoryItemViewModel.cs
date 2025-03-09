d76472b8d5ee4fd69715dbabccf3be8203dc2f8f
Mahmoud Samy <165038050+MahmooudSamy@users.noreply.github.com>
2025-03-09 05:07:59 +0200

    create inventory viewmodel

--- Invento/ViewModels/InventoryItemViewModel.cs
+++ Invento/ViewModels/InventoryItemViewModel.cs
@@ -1,18 +1,110 @@
-﻿using System;
+﻿using Invento.DataAccess.Data.Repositories;
+using Invento.Events;
+using Invento.Model;
+using Invento.Wrapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Invento.ViewModels
 {
     public class InventoryItemViewModel : ViewModelBase, IInventoryItemViewModel
     {
-        public bool HasChanges { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        public Task AddEditInventoryItem(int? itemId)
+        private IInventoryItemRepository _inventoryItemRepository;
+        private IEventAggregator _eventAggregator;
+        private InventoryItemWrapper _inventoryitemwrapper;
+        private bool _haschanges;
+        public InventoryItemViewModel(IInventoryItemRepository inventoryItemRepository,IEventAggregator eventAggregator)
         {
-            throw new NotImplementedException();
+            _inventoryItemRepository = inventoryItemRepository;
+            _eventAggregator = eventAggregator;
+            SaveCommand = new DelegateCommand(OnSaveExecute, OnSaveCanExecute);
         }
+
+        private bool OnSaveCanExecute()
+        {
+            return InventoryItemWrapper != null && !InventoryItemWrapper.HasErrors && HasChanges;
+        }
+
+        private async void OnSaveExecute()
+        {
+            try
+            {
+                await _inventoryItemRepository.SaveAsync();
+                HasChanges = _inventoryItemRepository.HasChanges();
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+       
+
+        public async Task AddEditInventoryItem(int? itemId)
+        {
+            var item = itemId.HasValue
+               ? await _inventoryItemRepository.GetInventoryItemAsyncById(itemId.Value)
+               : CreateNewItem();
+            InitilaizeItem(item);
+            //if (InventoryItemWrapper.Id == 0)
+            //{
+            //    InventoryItemWrapper.ItemName = "";
+
+            //}
+        }
+
+        private void InitilaizeItem(InventoryItem item)
+        {
+            InventoryItemWrapper = new InventoryItemWrapper(item);
+            InventoryItemWrapper.PropertyChanged += (s, e) =>
+            {
+                if (!HasChanges)
+                {
+                    HasChanges = _inventoryItemRepository.HasChanges();
+                }
+                if (e.PropertyName == nameof(InventoryItemWrapper.HasErrors))
+                {
+                    ((DelegateCommand)SaveCommand).RaiseCanExecuteChanged();
+                }
+            };
+            ((DelegateCommand)SaveCommand).RaiseCanExecuteChanged();
+        }
+
+        private InventoryItem CreateNewItem()
+        {
+            var inventoryitem = new InventoryItem();
+            _inventoryItemRepository.Add(inventoryitem);
+            return inventoryitem;
+        }
+
+        public InventoryItemWrapper InventoryItemWrapper
+        {
+            get { return _inventoryitemwrapper; }
+            set { _inventoryitemwrapper = value; OnPropertyChanged(); }
+        }
+        
+
+        public bool HasChanges
+        {
+            get { return _haschanges; }
+            set
+            {
+                if (_haschanges != value)
+                {
+                    _haschanges = value;
+                    OnPropertyChanged();
+                    ((DelegateCommand)SaveCommand).RaiseCanExecuteChanged();
+                }
+
+            }
+        }
+
+        public ICommand  SaveCommand { get; }
     }
 }
